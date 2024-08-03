a716bf70425ea1165fb128c270851e6aa85c21b8
klasmer <klasmer495@gmail.com>
2024-08-03 15:59:55 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        byte variableNumbers1 = 127;
+        System.out.println("значение переменной byte = " + variableNumbers1);
+        int variableNumbers2 =2_147_483_647;
+        System.out.println("значение переменной int = " + variableNumbers2);
+        short variableNumbers3 = 32_767;
+        System.out.println("значение переменной short = " + variableNumbers3);
+        long variableNumbers4 = 9_223_372_036_854_775_807L;
+        System.out.println("значение переменной long = " + variableNumbers4);
+        float variableNumbers5 = 3.4E+38f;
+        System.out.println("значение переменной float = " + variableNumbers5);
+        double variableNumbers6 = 1.7E+308;
+        System.out.println("значение переменной double = " + variableNumbers6);
+
     }
 }
